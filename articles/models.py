bd668d39fe3d1554823d0d08d8d3afb7976746bc
JCMeloza <jcarlos@SysMijas.localdomain>
2026-06-11 20:45:11 +0200

    slug, contenido, imagen, estado y marcas de tiempo; se creó la migración inicial para el modelo Artículo. Se actualizó el admin para registrar el modelo de usuario personalizado con opciones de visualización adicionales.

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,38 @@
 # Maneja los artículos, categorías, etiquetas, comentarios y la lógica de los "Me gusta".
+from email.policy import default
+from enum import unique
+
+
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Article(models.Model):
+    STATUS_CHOICES=[
+        ('draft', 'Borrador'),
+        ('pending', 'En Revisión'),
+        ('published', 'Publicado'),
+        ('rejected', 'Rechazado'),
+    ]
+
+    autor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='articles'
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    image= models.ImageField(upload_to='articles/', null= True, blank=True)
+
+    status= models.CharField( 
+        max_length=10,
+        choices = STATUS_CHOICES,
+        default='draft'    
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at= models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
