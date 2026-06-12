0b03f05812e99a7acbbbc22c0131324233b4feb1
JuanCarlos <melozajuancarlos@gmail.com>
2026-06-12 13:34:49 +0200

    Agregar vistas y rutas para registro, inicio de sesión y cierre de sesión de usuarios.

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import ArticleListView
+from .views import ArticleListView, RegisterView, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
     path('about/', TemplateView.as_view(template_name='general/acerca_de.html'), name='about'),
     path('contact/', TemplateView.as_view(template_name='general/contacto.html'), name='contact'),
+
+    path('registro/', RegisterView.as_view(), name='registro'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 from .models import Article
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 class ArticleListView(ListView):
@@ -9,3 +12,15 @@ class ArticleListView(ListView):
 
     def get_queryset(self):
         return Article.objects.filter(status='published').select_related('autor', 'category').order_by('-created_at')
+
+# Vista para registro de usuarios
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/registro.html'
+    succes_url = reverse_lazy('login')
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('home')
